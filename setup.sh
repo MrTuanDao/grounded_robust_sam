79084d6c559933be495a611a850ec8536aa7fd7c
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2024-09-16 14:57:15 +0700

    a

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 python -m venv env
 source env/bin/activate
 pip install torch==2.0.1+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install wheel
 pip install --no-build-isolation -e GroundingDINO
 pip install -r requirements.txt
 
