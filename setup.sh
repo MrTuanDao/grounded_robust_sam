972e62937e590d15865c77d35fa9712770fd765c
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2024-09-16 14:50:00 +0700

    a

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pip install torch==2.1.0 wheel numpy==1.24.1
+pip install python -m pip install torch==2.0.1+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install --no-build-isolation -e GroundingDINO
 pip install -r requirements.txt
 
-wget https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
-wget https://huggingface.co/robustsam/robustsam/resolve/main/model_checkpoint/robustsam_checkpoint_l.pth
+# wget https://huggingface.co/ShilongLiu/GroundingDINO/resolve/main/groundingdino_swint_ogc.pth
+# wget https://huggingface.co/robustsam/robustsam/resolve/main/model_checkpoint/robustsam_checkpoint_l.pth
